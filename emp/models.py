671567f19e763b9a84bab0510823bfef785203e4
Ravi <ravinder9.reddy@gmail.com>
2020-06-05 13:56:19 +0100

    Build base app for future learning 	modified:   emp/models.py 	new file:   emp/urls.py 	modified:   emp/views.py 	modified:   employee/settings.py 	modified:   employee/urls.py

--- emp/models.py
+++ emp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Employee(models.Model):
+    employee_name = models.CharField(max_length=200)
+    employee_dept = models.CharField(max_length=10)
+
+
+
